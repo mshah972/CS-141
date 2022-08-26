e62d28d8e0b55e4f88ff144b8a50ce8f86ad4d92
mshah245 <93416417+mshah972@users.noreply.github.com>
2022-08-26 12:43:04 -0500

    Program 1 Starting

---
+++ Program 1/main.cpp
@@ -0,0 +1,16 @@
+/*-------------------------------------------
+Program 1: Basic Calculator
+Course: CS 141, Fall 2022, UIC
+System: MacOS
+Author: Moksh Shah, starter code provided by Ellen Kidane
+------------------------------------------- */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
